b048cb9bbd4bc5977dda609370b6d045c84ee931
rlama7 <royalray7@gmail.com>
2019-02-13 20:19:06 -0800

    First NodeJS Expresss app.

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('Hello World');
+// jshint esverison: 6
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// get requeset
+app.get('/', (req, res) => res.send('<h1>Hello World! Happy Programming!!</h1>'));
+
+// aboutme
+app.get('/aboutme', (req, res) => res.send('<h1>About Me: I love programming!</h1>'));
+
+// contactme
+app.get('/contactme', (req, res) => res.send('<h1>Contact Me: contact me at: rlama7@mail.sfsu.edu</h1>'));
+
+// listen from express module
+app.listen(port, () => console.log('app listening on port ${port}!...'));
